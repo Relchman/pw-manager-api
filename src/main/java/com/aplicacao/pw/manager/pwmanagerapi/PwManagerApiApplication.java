936c698e702a606aba474de1c2d9c4359b59eeb6
relchman <relchmanaugusto@gmail.com>
2023-12-18 10:52:57 -0300

    feat(project):hammer::Inicialização de projeto

---
+++ src/main/java/com/aplicacao/pw/manager/pwmanagerapi/PwManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacao.pw.manager.pwmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicacao/pw/manager/pwmanagerapi/PwManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacao.pw.manager.pwmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
